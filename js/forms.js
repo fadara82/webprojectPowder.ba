f4a44eb19876c681a113f3ac3181d0240e213a73
fadara82 <92827557+fadara82@users.noreply.github.com>
2024-03-31 22:27:00 +0200

    Add files via upload

---
+++ js/forms.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  $("#regform").validate({
+    rules: {
+      fname: {
+        required: true,
+        minlength: 2, // Adjust minlength according to your requirement
+      },
+      lname: {
+        required: true,
+        minlength: 2, // Adjust minlength according to your requirement
+      },
+      email: {
+        required: true,
+        email: true, // Adjust minlength according to your requirement
+        minlength: 5,
+      },
+      password: {
+        minlength: 6,
+        required: true,
+      },
+      cpassword: {
+        required: true,
+        equalTo: "#floatingPassword",
+      },
+    },
+    messages: {
+      fname: {
+        required: "Please enter your first name",
+        minlength: "First name must be at least 2 characters long", // Adjust error message as needed
+      },
+      lname: {
+        required: "Please enter your last name",
+        minlength: "Last name must be at least 2 characters long", // Adjust error message as needed
+      },
+      email: {
+        required: "Please enter your email",
+        email: "Enter valid email",
+      },
+      confirmPassword: {
+        required: "Enter password",
+        equalTo: "Must be same as password",
+      },
+    },
+    submitHandler: function (form, event) {
+      event.preventDefault();
+      blockUi("#regform", 3000);
+    },
+  });
+});
+
+blockUi = (element, timeout) => {
+  $(element).block({
+    message: '<div id="throbber"></div>',
+    timeout: timeout,
+  });
+};
